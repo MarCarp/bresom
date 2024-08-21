c8e8abf06ea431af97aeccb9b65df564495f483b
Marco Carpentier <marcarp@protonmail.com>
2024-08-21 21:00:41 +0200

    Update template and create persos template

--- common.js
+++ common.js
@@ -9,6 +9,15 @@ function fadeOutBeforeChangePage(ev){
     }, 1000);
 }
 
+document.addEventListener("keypress",function(event){
+    if(event.code === "Space") {
+        document.body.classList.add("fade");
+    const fadeAway = setTimeout(() => {
+        location.href = targetPage;
+    }, 1000);
+    }
+});
+
 for(link of links){
     link.addEventListener("click",fadeOutBeforeChangePage);
 }
