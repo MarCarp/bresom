5c377741503e6cc30801bea23158d5e9c526bcb6
Marco Carpentier <marcarp@protonmail.com>
2024-08-22 03:14:20 +0200

    Move Script Update fader Add player view Clean files

--- scripts/common.js
+++ scripts/common.js
@@ -3,21 +3,23 @@ const links = document.getElementsByTagName("a");
 function fadeOutBeforeChangePage(ev){
     const targetPage = ev.target.href;
     ev.preventDefault();
-    document.body.classList.add("fade");
+    document.body.classList.add("fade-out");
     const fadeAway = setTimeout(() => {
         location.href = targetPage;
     }, 1000);
 }
 
-document.addEventListener("keypress",function(event){
-    if(event.code === "Space") {
-        document.body.classList.add("fade");
-    const fadeAway = setTimeout(() => {
-        location.href = targetPage;
-    }, 1000);
-    }
-});
+function cleanBody() {
+    document.body.classList.remove("fade-out");
+}
+
+function init() {
+    console.log("Page Loaded");
+}
 
 for(link of links){
     link.addEventListener("click",fadeOutBeforeChangePage);
 }
+
+window.addEventListener("pageshow", cleanBody);
+document.addEventListener("DOMContentLoaded", init);
