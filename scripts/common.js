4a05639e01eae864994babcbd70b69de1d62a0b2
Marco Carpentier <marcarp@protonmail.com>
2024-08-24 19:08:34 +0200

    Update damages popin Update adrenaline shot feature

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,20 @@
+/*var splide = new Splide( '.splide', {
+    type   : 'loop',
+    direction: 'ttb',
+    height   : '80px',
+    drag   : 'free',
+    snap   : true,
+    perPage: 1,
+    pagination: false
+  } );
+
+splide.mount();*/
+
 const links = document.getElementsByTagName("a");
+
+// DOM
+const mainModal = document.getElementById("modal-wrap");
+const popOutCTA = document.getElementById("modal-close");
 
 function fadeOutBeforeChangePage(ev){
     const targetPage = ev.target.href;
@@ -13,13 +29,20 @@ function cleanBody() {
     document.body.classList.remove("fade-out");
 }
 
+function popOut() {
+    mainModal.classList.remove("active");
+}
+
 function init() {
-    console.log("Page Loaded");
+    console.log("RAS");
 }
 
 for(link of links){
     link.addEventListener("click",fadeOutBeforeChangePage);
 }
+
+// EVENT LISTENERS
+popOutCTA.addEventListener("click", popOut);
 
 window.addEventListener("pageshow", cleanBody);
 document.addEventListener("DOMContentLoaded", init);
--- scripts/play.js
+++ scripts/play.js
@@ -1,3 +1,4 @@
+// VARIABLES
 const maxPoints = 12;
 
 let mentalHealth = maxPoints;
@@ -5,13 +6,24 @@ let physicHealth = maxPoints;
 let adrenaline = 3;
 
 // DOM
+// DOM - COUNTERS
 const counterEsprit = document.getElementById("counter-esp");
 const counterPhysic = document.getElementById("counter-phy");
-
+// DOM - CHECKBOXES
 const checkboxesEsprit = document.querySelectorAll("[data-espr]");
 const checkboxesPhysic = document.querySelectorAll("[data-phys]");
 const checkboxesAdre = document.querySelectorAll("[data-adre]");
+// DOM - DMG COUNTER
+const espDmgCounter = document.getElementById("esp-dmg-counter");
+const phyDmgCounter = document.getElementById("phy-dmg-counter");
+// DOM - CTAS
+const chockBtn = document.getElementById("btn-damages");
+const dmgApply = document.getElementById("dmg-apply");
+const adreShotBtn = document.getElementById("btn-adrenaline");
+// DOM - OTHERS
+const modalElement = document.getElementById("modal-wrap");
 
+// SORT ARRAYS
 const tableEsprit = [];
 for(espritElem of checkboxesEsprit) {
     espritElem.addEventListener("change", updateScore);
@@ -24,6 +36,35 @@ for(physicElem of checkboxesPhysic) {
     tablePhysic[parseInt(physicElem.dataset.phys)] = physicElem;
 }
 
+// FUnCtions
+function popModal() {
+    modalElement.classList.add("active");
+    espDmgCounter.value = 0;
+    phyDmgCounter.value = 0;
+}
+
+function applyDamages() {
+    if(phyDmgCounter !== 0) {
+        physicHealth -= phyDmgCounter.value;
+        updtateDOMcounter("phy");
+    }
+    if(espDmgCounter !== 0) {
+        mentalHealth -= espDmgCounter.value;
+        updtateDOMcounter("esp");
+    }
+    popOut();
+}
+
+function adrenalineShot() {
+    for(adrEl of checkboxesAdre) {
+        if(!adrEl.classList.contains("close") && !adrEl.checked) {
+            adrEl.checked = true;
+            updateAdrenaline();
+            break;
+        }
+    }
+}
+
 function updateAdrenaline() {
     if(physicHealth <= 8) {
         checkboxesAdre[1].classList.remove("close");
@@ -31,6 +72,32 @@ function updateAdrenaline() {
     if(physicHealth <= 4) {
         checkboxesAdre[2].classList.remove("close");
     }
+    for(adrEl of checkboxesAdre) {
+        if(!adrEl.classList.contains("close") && !adrEl.checked) {
+            adreShotBtn.disabled = false;
+            break;
+        } else {
+            adreShotBtn.disabled = true;
+        }
+    }
+}
+
+function damageUpdate(char, dir) {
+    let target = phyDmgCounter;
+    let damages = parseInt(target.value);
+    let max = physicHealth;
+    if (char==="esp"){
+        target = espDmgCounter;
+        damages = parseInt(target.value);
+        max = mentalHealth;
+    }
+    if(dir === "up" && damages < max) {
+        damages += 1;
+    }
+    else if (dir === "down" && damages > 0) {
+        damages -= 1;
+    }
+    target.value = damages;
 }
 
 function updtateDOMcounter (cat) {
@@ -39,12 +106,14 @@ function updtateDOMcounter (cat) {
             tableEsprit[indexEsp].checked = indexEsp >= mentalHealth;
         }
         counterEsprit.innerText = mentalHealth;
+        espDmgCounter.setAttribute("max", mentalHealth);
     }
     if(cat === "phy" || cat ==="both") {
         for(indexPhy in tablePhysic) {
             tablePhysic[indexPhy].checked = indexPhy >= physicHealth;
         }
         counterPhysic.innerText = physicHealth;
+        phyDmgCounter.setAttribute("max", physicHealth);
         updateAdrenaline();
     }
 }
@@ -58,6 +127,11 @@ function updateScore(ev) {
         updtateDOMcounter("phy");
     }
 }
+
+// EVENT LISTENERS
+chockBtn.addEventListener("click", popModal);
+dmgApply.addEventListener("click", applyDamages)
+adreShotBtn.addEventListener("click", adrenalineShot);
 
 // INIT
 updtateDOMcounter("both");
